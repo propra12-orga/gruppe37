0724c851630114af8853544b7232be8051983b06
Johannes Büker <Johannes@Hannes.(none)>
2012-06-21 11:39:20 +0200

    Levelauswahl hat nun Dateifilter auf xml-Files

--- src/Gui/OeffnenDialogClass.java
+++ src/Gui/OeffnenDialogClass.java
@@ -3,14 +3,19 @@ package Gui;
 import java.io.File;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class OeffnenDialogClass {
 	private final Main window;
 
 	private void oeffnen() {
+		FileFilter filter = new FileNameExtensionFilter("Leveldateien", "xml");
+
 		JFileChooser chooser = new JFileChooser(new File(
 				System.getProperty("user.dir")));
 		chooser.setDialogType(JFileChooser.OPEN_DIALOG);
+		chooser.addChoosableFileFilter(filter);
 
 		chooser.setVisible(true);
 		chooser.showOpenDialog(null);
